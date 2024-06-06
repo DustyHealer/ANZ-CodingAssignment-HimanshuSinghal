8e27fab23affafb0aa964f7d884c8e5282e2f1e0
Himanshu Singhal <developer.singhal@gmail.com>
2024-06-06 18:09:57 +0530

    Initial Commit

---
+++ RoboToy/Program.cs
@@ -0,0 +1,71 @@
+﻿using RoboToy.Controller;
+
+namespace RoboToy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var robot = new ToyController();
+
+                while (true)
+                {
+                    string line = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        break;
+                    }
+
+                    line = line.ToUpper();
+                    string[] input = line.Split(' ');
+                    string command = input[0].Trim();
+
+                    switch (command)
+                    {
+                        case "PLACE":
+                            if (string.IsNullOrEmpty(input[1]))
+                            {
+                                Console.WriteLine("Invalid command format.");
+                                continue;
+                            }
+
+                            string[] parts = input[1].Trim().Split(',');
+                            if (parts.Length != 3)
+                            {
+                                Console.WriteLine("Invalid command format.");
+                                continue;
+                            }
+
+                            int x = int.Parse(parts[0].Trim());
+                            int y = int.Parse(parts[1].Trim());
+                            ToyController.Direction facing = (ToyController.Direction)Enum.Parse(typeof(ToyController.Direction), parts[2].Trim());
+                            robot.Place(x, y, facing);
+                            break;
+                        case "MOVE":
+                            robot.Move();
+                            break;
+                        case "LEFT":
+                            robot.Left();
+                            break;
+                        case "RIGHT":
+                            robot.Right();
+                            break;
+                        case "REPORT":
+                            Console.WriteLine(robot.Report());
+                            break;
+                        default:
+                            Console.WriteLine("Invalid command.");
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception Occurred: {e.Message}, Exiting...");
+            }
+        }
+    }
+}
---
+++ RoboToyApp.nUnitTests/ToyControllerTests.cs
@@ -0,0 +1,256 @@
+using RoboToy.Controller;
+using static RoboToy.Controller.ToyController;
+
+namespace RoboToyApp.nUnitTests
+{
+    public class ToyControllerTests
+    {
+        private ToyController _toyController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _toyController = new ToyController();
+        }
+
+        [Test]
+        public void ToyController_IsInitiallyNotPlaced()
+        {
+            // Act
+            var result = _toyController.Report();
+
+            // Assert
+            Assert.That(result.Equals("Robot is not placed on the table."));
+        }
+
+        [Test]
+        public void ToyController_Place_ValidPosition_SetsProperties()
+        {
+            // Act
+            _toyController.Place(1, 2, Direction.EAST);
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("1"));
+            Assert.That(output[1].Equals("2"));
+            Assert.That(output[2].Equals("EAST"));
+        }
+
+        [Test]
+        public void ToyController_Place_InvalidPosition_DoesntChangeState()
+        {
+            // Act
+            _toyController.Place(-1, 3, Direction.WEST); // -1 is not a valid position
+            var result = _toyController.Report();
+
+            // Assert
+            Assert.That(result.Equals("Robot is not placed on the table."));
+        }
+
+        [Test]
+        public void ToyController_Move_NotPlaced_DoesNothing()
+        {
+            // Act
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            // Assert
+            Assert.That(result.Equals("Robot is not placed on the table."));
+        }
+
+        [Test]
+        public void ToyController_Move_North_UpdatesY()
+        {
+            // Act
+            _toyController.Place(0, 0, Direction.NORTH);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("0")); // Unchanged X value
+            Assert.That(output[1].Equals("1")); // Updates the Y value
+            Assert.That(output[2].Equals("NORTH"));
+        }
+
+        [Test]
+        public void ToyController_Move_East_UpdatesX()
+        {
+            // Act
+            _toyController.Place(0, 0, Direction.EAST);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("1")); // Updates the X value
+            Assert.That(output[1].Equals("0")); // Unchanged Y value
+            Assert.That(output[2].Equals("EAST"));
+        }
+
+        [Test]
+        public void ToyController_Move_South_UpdatesY()
+        {
+            // Act
+            _toyController.Place(0, 1, Direction.SOUTH);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("0")); // Unchanged X value
+            Assert.That(output[1].Equals("0")); // Updates the Y value
+            Assert.That(output[2].Equals("SOUTH"));
+        }
+
+        [Test]
+        public void ToyController_Move_West_UpdatesX()
+        {
+            // Act
+            _toyController.Place(1, 0, Direction.WEST);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("0")); // Updates the X value
+            Assert.That(output[1].Equals("0")); // Unchanged Y value
+            Assert.That(output[2].Equals("WEST"));
+        }
+
+        [Test]
+        public void ToyController_Move_WontFallOffTable_North()
+        {
+            // Act
+            _toyController.Place(0, 4, Direction.NORTH);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("0")); // Unchanged X value
+            Assert.That(output[1].Equals("4")); // Unchanged Y value
+            Assert.That(output[2].Equals("NORTH"));
+        }
+
+        [Test]
+        public void ToyController_Move_WontFallOffTable_East()
+        {
+            // Act
+            _toyController.Place(4, 0, Direction.EAST);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("4")); // Unchanged X value
+            Assert.That(output[1].Equals("0")); // Unchanged Y value
+            Assert.That(output[2].Equals("EAST"));
+        }
+
+        [Test]
+        public void ToyController_Move_WontFallOffTable_South()
+        {
+            // Act
+            _toyController.Place(3, 0, Direction.SOUTH);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("3")); // Unchanged X value
+            Assert.That(output[1].Equals("0")); // Unchanged Y value
+            Assert.That(output[2].Equals("SOUTH"));
+        }
+
+        [Test]
+        public void ToyController_Move_WontFallOffTable_West()
+        {
+            // Act
+            _toyController.Place(0, 3, Direction.WEST);
+            _toyController.Move();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("0")); // Unchanged X value
+            Assert.That(output[1].Equals("3")); // Unchanged Y value
+            Assert.That(output[2].Equals("WEST"));
+        }
+
+        [Test]
+        public void Left_RobotNotPlaced_DoesNotRotate()
+        {
+            // Act
+            _toyController.Left();
+            var result = _toyController.Report();
+            
+            // Assert
+            Assert.That(result.Equals("Robot is not placed on the table."));
+        }
+
+        [Test]
+        public void Left_RobotPlacedFacingNorth_RotatesToWest()
+        {
+            // Act
+            _toyController.Place(2, 2, Direction.NORTH);
+            _toyController.Left();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("2")); // Unchanged X value
+            Assert.That(output[1].Equals("2")); // Unchanged Y value
+            Assert.That(output[2].Equals("WEST")); // Direction Updated
+        }
+
+        [Test]
+        public void Right_RobotNotPlaced_DoesNotRotate()
+        {
+            // Act
+            _toyController.Right();
+            var result = _toyController.Report();
+
+            // Assert
+            Assert.That(result.Equals("Robot is not placed on the table."));
+        }
+
+        [Test]
+        public void Right_RobotPlacedFacingNorth_RotatesToEast()
+        {
+            // Act
+            _toyController.Place(2, 2, Direction.NORTH);
+            _toyController.Right();
+            var result = _toyController.Report();
+
+            string[] output = result.Split(',');
+
+            // Assert
+            Assert.That(output.Length.Equals(3));
+            Assert.That(output[0].Equals("2")); // Unchanged X value
+            Assert.That(output[1].Equals("2")); // Unchanged Y value
+            Assert.That(output[2].Equals("EAST"));
+        }
+    }
+}
